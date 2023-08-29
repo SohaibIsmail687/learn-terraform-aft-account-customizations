8c8fb1d3d3582ccb76910571605d35e36f9c541e
Sohaib Ismail <sohaib.ismail@atomic-computing.com>
2023-08-29 15:28:23 +0500

    Creating ssm parameters

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -1,6 +1,6 @@
 data "aws_caller_identity" "current" {}
 
-resource "aws_s3_bucket" "sandbox_bucketT" {
+resource "aws_s3_bucket" "sandbox_buckett" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
